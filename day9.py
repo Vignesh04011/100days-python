ee4771848e69c1d9186e02ef2d7f5e534efd9ac1
Vignesh Vane <vigneshvane200@gmail.com>
2026-02-24 00:13:46 +0530

    day 9 is over

--- day9.py
+++ day9.py
@@ -1,2 +1,13 @@
-name="Vignesh"
+name="Vignesh Vane"
 print(len(name))
+print(name.upper())
+print(name.lower())
+print("Vane"in name)
+
+sentence="AI is powerful"
+words= sentence.split()
+print(words)
+
+
+name=input("Enter name:")
+print("Length is:",len(name))
