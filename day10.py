94d8ad17fe3fcd77760ca67b54771345f29b6ab8
Vignesh Vane <vigneshvane200@gmail.com>
2026-02-25 00:01:49 +0530

    day 10 is over

---
+++ day10.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+print(math.sqrt(16))
+print(math.factorial(5))    
+
+print(random.randint(1,10))
+
+names=["AI","is","Powerful"]
+
+print(random.choice(names))
+
+print("Dice rolled :",random.randint(1,6))
